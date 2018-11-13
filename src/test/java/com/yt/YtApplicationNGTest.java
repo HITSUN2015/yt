6ab6afb984c167fdb4f270c7c8cb57688370a0f2
yantong <sun18701689022@gmail.com>
2018-11-13 19:32:57 +0800

    [add]配置文件 变成 yaml格式。添加 testng功能的测试代码，经测试 可自动生成基本的报告文件

---
+++ src/test/java/com/yt/YtApplicationNGTest.java
@@ -0,0 +1,25 @@
+package com.yt;
+
+import com.yt.db.mybatis.MybatisOperationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * testng demo
+ * idea 的话 需要 在右上角 配置那里 ->configuration->listeners->use default reporters打勾 才能自动生成 报告文件
+ * Created by yantong on 2018/11/13.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class YtApplicationNGTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private MybatisOperationService mybatisOperationService;
+
+    @Test
+    public void getHealthCheckIntegrationTest() throws Exception{
+        int addedCount = mybatisOperationService.addDuiXiang(null);
+        Assert.assertEquals(0, addedCount);
+    }
+}
