b28df1e9d8b5a67bcf983749a05b54e30089f7c6
yantong <sun18701689022@gmail.com>
2018-11-14 20:38:56 +0800

    [add]修改 配置文件 将 已className获取到的logger 信息输出到本地日志文件

--- src/main/java/com/yt/db/mybatis/MybatisOperationServiceImpl.java
+++ src/main/java/com/yt/db/mybatis/MybatisOperationServiceImpl.java
@@ -14,12 +14,16 @@ import org.springframework.stereotype.Service;
 public class MybatisOperationServiceImpl implements MybatisOperationService {
 
     private static org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger("fileLogger");
+    private static org.slf4j.Logger classSlf4jLogger = LoggerFactory.getLogger(MybatisOperationServiceImpl.class);
     private static org.apache.logging.log4j. Logger log4j2Logger = LogManager.getLogger("fileLogger");
+    private static org.apache.logging.log4j. Logger classLog4j2Logger = LogManager.getLogger(MybatisOperationServiceImpl.class);
 
     @Override
     public int addDuiXiang(DuiXiang duiXiang) {
-        slf4jLogger.error("slf4jLogger test log4j2");
-        log4j2Logger.error("log4j2Logger test log4j2");
+        slf4jLogger.error("slf4jLogger test slf4jLogger");
+        classSlf4jLogger.error("classSlf4jLogger test classSlf4jLogger");
+        log4j2Logger.error("log4j2Logger test log4j2Logger");
+        classLog4j2Logger.error("classLog4j2Logger test classLog4j2Logger");
         return 0;
     }
 }
