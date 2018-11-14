0bc6380bf64a3698d35e3460043d58788a9208a0
yantong <sun18701689022@gmail.com>
2018-11-14 20:11:30 +0800

    [add]添加 log4j2 slf4j 来记录日志

--- src/main/java/com/yt/db/mybatis/MybatisOperationServiceImpl.java
+++ src/main/java/com/yt/db/mybatis/MybatisOperationServiceImpl.java
@@ -1,6 +1,10 @@
 package com.yt.db.mybatis;
 
 import com.yt.db.mybatis.domain.DuiXiang;
+//import org.apache.logging.log4j.Logger;Logger
+import org.apache.logging.log4j.LogManager;
+//import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,9 +13,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class MybatisOperationServiceImpl implements MybatisOperationService {
 
+    private static org.slf4j.Logger slf4jLogger = LoggerFactory.getLogger("fileLogger");
+    private static org.apache.logging.log4j. Logger log4j2Logger = LogManager.getLogger("fileLogger");
+
     @Override
     public int addDuiXiang(DuiXiang duiXiang) {
-
+        slf4jLogger.error("slf4jLogger test log4j2");
+        log4j2Logger.error("log4j2Logger test log4j2");
         return 0;
     }
 }
