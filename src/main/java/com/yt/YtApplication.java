68536580336a663d50694f814e12bb0e3935e300
yantong <sun18701689022@gmail.com>
2018-11-22 19:02:39 +0800

    [add]test quartz and file util for delete

--- src/main/java/com/yt/YtApplication.java
+++ src/main/java/com/yt/YtApplication.java
@@ -1,9 +1,12 @@
 package com.yt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.yt")//spring bean加载 扫描的根路径
+@ImportResource({"classpath:quartz/quartz.xml"})
 public class YtApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/yt/tool/FileUtilTest.java
@@ -0,0 +1,18 @@
+package com.yt.tool;
+
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Created by yantong on 2018/11/22.
+ */
+public class FileUtilTest {
+
+    @Test
+    public void testDelete(){
+        List<File> files = FileUtil.syncListFileByPath("D:\\tmp\\test");
+        FileUtil.sycnDeleteFilesBeforeMinutesByLastModifyTime(files, 1);
+    }
+}
