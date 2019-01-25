159f54d641009ceb478128137b0a2d06691042d1
yantong <sun18701689022@gmail.com>
2019-01-25 17:30:18 +0800

    [add]测试 poolthread

---
+++ yt-util/src/main/java/com/yt/util/async/springtask/ThreadPoolConfig.java
@@ -0,0 +1,29 @@
+package com.yt.util.async.springtask;
+
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * Created by yantong on 2019/1/25.
+ */
+@Configuration
+public class ThreadPoolConfig extends WebMvcConfigurerAdapter {
+
+    @Bean(autowire = Autowire.BY_NAME)
+    public ThreadPoolTaskExecutor testExecutor() {
+        ThreadPoolTaskExecutor pool = new ThreadPoolTaskExecutor();
+        pool.setCorePoolSize(20);
+        pool.setQueueCapacity(200);
+        pool.setMaxPoolSize(50);
+        pool.setRejectedExecutionHandler(new ThreadPoolExecutor.DiscardPolicy());
+        pool.setKeepAliveSeconds(10);
+        pool.setThreadNamePrefix("test-");
+        return pool;
+    }
+
+}
