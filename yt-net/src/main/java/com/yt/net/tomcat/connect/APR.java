db4033f658ffd680535c476a794ae4147752a8df
yantong <sun18701689022@gmail.com>
2019-02-16 16:26:40 +0800

    [add]测试 net

---
+++ yt-net/src/main/java/com/yt/net/tomcat/connect/APR.java
@@ -0,0 +1,13 @@
+package com.yt.net.tomcat.connect;
+
+/**
+ * Created by yantong on 2019/2/16.
+ *
+ * spring boot support apr https://www.jianshu.com/p/f716726ba340
+ *
+ * 相比NIO 有的说 提升 20%
+ *
+ * 具体原理应该是 用C实现了NIO
+ */
+public class APR {
+}
---
+++ yt-net/src/main/java/com/yt/net/tomcat/connect/Introduce.java
@@ -0,0 +1,9 @@
+package com.yt.net.tomcat.connect;
+
+/**
+ * Created by yantong on 2019/2/16.
+ *
+ * https://www.ibm.com/developerworks/cn/java/j-lo-tomcat2/
+ */
+public class Introduce {
+}
