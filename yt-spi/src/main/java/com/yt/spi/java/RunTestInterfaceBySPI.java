e024938f5ba8bdd0fdebcddee49f3e8dbe19f8d7
yantong <sun18701689022@gmail.com>
2019-03-02 16:20:27 +0800

    [add]java spi

--- yt-spi/src/main/java/com/yt/spi/java/RunTestInterfaceBySPI.java
+++ yt-spi/src/main/java/com/yt/spi/java/RunTestInterfaceBySPI.java
@@ -15,6 +15,10 @@ import java.util.ServiceLoader;
  *
  * 样例：Mysql-connector-java:5.1.44 目录下有java.sql.Driver 文件
  *
+ * 缺点（随意抄的：https://www.jianshu.com/p/7daa38fc9711）：
+ *     虽然ServiceLoader也算是使用的延迟加载，但是基本只能通过遍历全部获取，也就是接口的实现类全部加载并实例化一遍。如果你并不想用某些实现类，它也被加载并实例化了，这就造成了浪费。
+ *     获取某个实现类的方式不够灵活，只能通过Iterator形式获取，不能根据某个参数来获取对应的实现类。
+ *
  * {@link ServiceLoader} 的
  * private static final String PREFIX = "META-INF/services/";
  *
