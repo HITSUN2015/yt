b5de3d3bc56879e2acec684b9ad4f6dbdcf83fca
yantong <sun18701689022@gmail.com>
2019-03-02 16:13:42 +0800

    [add]java spi

--- yt-spi/src/main/java/com/yt/spi/java/RunTestInterfaceBySPI.java
+++ yt-spi/src/main/java/com/yt/spi/java/RunTestInterfaceBySPI.java
@@ -5,7 +5,9 @@ import java.util.ServiceLoader;
 /**
  * Created by yantong on 2019/3/2.
  *
- * {@link sun.security.provider.ConfigFile.Spi}
+ * 介绍：SPI的全名为Service Provider Interface.大多数开发人员可能不熟悉，因为这个是针对厂商或者插件的。
+ *      为了实现在模块装配的时候能不在程序里动态指明，这就需要一种服务发现机制。
+ *      Java SPI就是提供这样的一个机制：为某个接口寻×××实现的机制。
  *
  * 条件1：目录：classPath ？未测试 或 resrouce/META-INF/services/
  *      2:文件：packagePath.className
