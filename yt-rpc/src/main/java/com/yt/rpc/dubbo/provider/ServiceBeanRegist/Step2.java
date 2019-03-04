9e2bf4a154eeda2bce5fde33e6b17b518e0a62b7
sun <sun18701689022@gmail.com>
2019-03-04 22:26:43 +0800

    [add]dubbo 测试

---
+++ yt-rpc/src/main/java/com/yt/rpc/dubbo/provider/ServiceBeanRegist/Step2.java
@@ -0,0 +1,12 @@
+package com.yt.rpc.dubbo.provider.ServiceBeanRegist;/**
+ * Created by SUN on 19/3/2.
+ */
+
+/**
+ * @author SUN
+ * @create 2019-03-02 上午12:12
+ * @desc
+ */
+public class Step2 {
+
+}
