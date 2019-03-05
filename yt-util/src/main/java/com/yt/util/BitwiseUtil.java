a0102534cb05a36b98d0032d986c21e546720e6f
yantong <sun18701689022@gmail.com>
2019-03-05 16:35:26 +0800

    [add]添加 给数字的某位设置值

---
+++ yt-util/src/main/java/com/yt/util/BitwiseUtil.java
@@ -0,0 +1,25 @@
+package com.yt.util;
+
+/**
+ * Created by yantong on 2019/3/5.
+ */
+public class BitwiseUtil {
+
+    //这个 接口 我还真没想好怎么提供比较好。。。值是0的时候 不能知道是哪一位
+    public int setBit(Integer source, Integer bit, Integer value) {
+        if (null == source && (null == bit || null == value)) {
+            throw new IllegalArgumentException();
+        }
+        if (null != value && value != 1 && value != 0) {
+            throw new IllegalArgumentException();
+        }
+        if (null == source) {
+            return bit * value;
+        }
+        if (null == bit || value == null) {
+            return source;
+        }
+        // TODO: 2019/3/5 这个实现比较蛋疼
+        return (source & (~bit)) | (bit * value);
+    }
+}
