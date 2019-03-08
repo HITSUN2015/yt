1fd1616585a1050652e5c7991d3dd470f5e71223
yantong <sun18701689022@gmail.com>
2019-03-08 17:17:43 +0800

    [add]添加reactor 相关介绍

--- yt-design-patterns/src/main/java/com/yt/design/pattern/reactor/Introduce.java
+++ yt-design-patterns/src/main/java/com/yt/design/pattern/reactor/Introduce.java
@@ -5,6 +5,9 @@ package com.yt.design.pattern.reactor;
  *
  * Doung Lee http://gee.cs.oswego.edu/dl/cpjslides/nio.pdf
  *
+ * https://github.com/tpn/pdfs/blob/master/Reactor%20-%20An%20Object%20Behavioral%20Pattern%20for%20Demultiplexing%20and%20Dispatching%20Handles%20for%20Synchronous%20Events.pdf
+ *
  */
 public @interface Introduce {
+
 }
