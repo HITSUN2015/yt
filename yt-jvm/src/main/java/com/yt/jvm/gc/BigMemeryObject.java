8afec5701af3b8c5f8f1974f4355f35e1c4989ce
yantong <sun18701689022@gmail.com>
2019-03-11 11:51:10 +0800

    [add]jvm

---
+++ yt-jvm/src/main/java/com/yt/jvm/gc/BigMemeryObject.java
@@ -0,0 +1,19 @@
+package com.yt.jvm.gc;
+
+/**
+ * Created by yantong on 2019/3/11.
+ */
+public class BigMemeryObject {
+    public Object instance = null;
+    private static final int _1MB = 1024 * 1024;
+
+    private byte[] bigSize = new byte[2 * _1MB];
+
+    public Object getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Object instance) {
+        this.instance = instance;
+    }
+}
---
+++ yt-jvm/src/test/java/com/yt/jvm/gc/ReferenceCountingTest.java
@@ -0,0 +1,24 @@
+package com.yt.jvm.gc;
+
+import com.yt.jvm.gc.source.ReferenceCounting;
+import org.testng.annotations.Test;
+
+/**
+ * Created by yantong on 2019/3/11.
+ */
+@Test
+public class ReferenceCountingTest {
+
+    @Test
+    public void testReferenceCounting() {
+        BigMemeryObject referenceCountingA = new BigMemeryObject();
+        BigMemeryObject referenceCountingB = new BigMemeryObject();
+        referenceCountingA.setInstance(referenceCountingB);
+        referenceCountingB.setInstance(referenceCountingA);
+
+        referenceCountingA = null;
+        referenceCountingB = null;
+
+        GCUtil.gc();
+    }
+}
