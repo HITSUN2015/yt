05f0939273fc61e91440d2f279fe5bf479c5b79f
yantong <sun18701689022@gmail.com>
2019-03-22 11:12:33 +0800

    [add]dubbo command 发送命令

---
+++ yt-rpc/src/main/java/com/yt/rpc/dubbo/exprience/Command.java
@@ -0,0 +1,12 @@
+package com.yt.rpc.dubbo.exprience;
+
+/**
+ * Created by yantong on 2019/3/22.
+ */
+public class Command {
+
+    /**
+     * telnet 127.0.0.1 20880
+     * invoke com.test.service.method("826251697")
+     */
+}
