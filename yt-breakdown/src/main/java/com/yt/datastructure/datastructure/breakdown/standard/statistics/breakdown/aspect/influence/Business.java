eae05a4d1c4d779d7c48ca575d6c3f3a38f1ac45
yantong <sun18701689022@gmail.com>
2019-03-26 11:30:10 +0800

    [add]整理

--- yt-breakdown/src/main/java/com/yt/datastructure/datastructure/breakdown/standard/statistics/breakdown/aspect/influence/Business.java
+++
@@ -1,9 +0,0 @@
-package com.yt.datastructure.datastructure.breakdown.standard.statistics.breakdown.aspect.influence;
-
-/**
- * Created by yantong on 2019/3/18.
- * 服务业务
- */
-public enum Business {
-    API_GATEWAY, BIG_DATA, APP_CLINET, AUTH, PUSH,
-}
--- yt-breakdown/src/main/java/com/yt/datastructure/datastructure/breakdown/standard/statistics/breakdown/aspect/influence/Level.java
+++
@@ -1,9 +0,0 @@
-package com.yt.datastructure.datastructure.breakdown.standard.statistics.breakdown.aspect.influence;
-
-/**
- * Created by yantong on 2019/3/18.
- * 可能还需要一个对象：影响范围
- */
-public enum Level {
-    ZERO, ONE, TWO, THREE, FOUR
-}
