0299599901c2f2d083c25017c1f1d6d9567f8bc8
yantong <sun18701689022@gmail.com>
2019-03-26 16:12:48 +0800

    [add]整理

--- yt-dataStruct/src/main/java/com/yt/datastructure/datastruct/Heap.java
+++
@@ -1,7 +0,0 @@
-package com.yt.datastructure.datastruct;
-
-/**
- * Created by yantong on 2019/2/14.
- */
-public class Heap {
-}
---
+++ yt-util/src/test/java/com/yt/jdk/thread/JoinTest.java
@@ -0,0 +1,16 @@
+package com.yt.jdk.thread;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by yantong on 2019/2/16.
+ */
+@Test
+public class JoinTest {
+
+    @Test
+    public void test() {
+        Join join = new Join();
+        join.demo();
+    }
+}
