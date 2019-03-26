186b2aa784a7e5f778a342278942086a4c27adbc
yantong <sun18701689022@gmail.com>
2019-03-26 15:53:29 +0800

    [add]介绍http头相关

---
+++ yt-rpc/src/test/java/com/yt/rpc/protocol/http/impl/JDKHttpTest.java
@@ -0,0 +1,14 @@
+package com.yt.rpc.protocol.http.impl;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by yantong on 2019/3/26.
+ */
+public class JDKHttpTest {
+
+    @Test
+    public void testSetHeader() {
+
+    }
+}
