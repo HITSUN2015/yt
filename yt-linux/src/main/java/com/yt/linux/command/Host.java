fadd745d48dbff894d5e7fffa4378a502294695c
yantong <sun18701689022@gmail.com>
2019-03-27 15:42:06 +0800

    [add]host 命令

---
+++ yt-linux/src/main/java/com/yt/linux/command/Host.java
@@ -0,0 +1,19 @@
+package com.yt.linux.command;
+
+/**
+ * Created by yantong on 2019/3/27.
+ */
+public class Host {
+
+    /**
+     * 查询 某个域名 绑定的 ip
+     * host -t a baidu.com
+     *
+     * 一个域名会绑定多个ip地址：
+     * baidu.com has address 220.181.57.216
+     * baidu.com has address 123.125.114.144
+     *
+     * 并且 在 调用 ping命令时，以上两个ip地址 都有可能出现
+     */
+
+}
