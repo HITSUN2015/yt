273e3a218384ef57277a4153f22b604aa9173381
yantong <sun18701689022@gmail.com>
2019-03-28 12:22:18 +0800

    添加锁 相关

---
+++ yt-java/src/main/java/com/yt/jdk/thread/lock/jdk/StampedLockDemo.java
@@ -0,0 +1,12 @@
+package com.yt.jdk.thread.lock.jdk;
+
+/**
+ * Created by yantong on 2019/3/28.
+ */
+public class StampedLockDemo implements LockDemo {
+
+    @Override
+    public void demo() {
+
+    }
+}
---
+++ yt-java/src/main/java/com/yt/jdk/thread/lock/jdk/aspect/Fair.java
@@ -0,0 +1,9 @@
+package com.yt.jdk.thread.lock.jdk.aspect;
+
+/**
+ * Created by yantong on 2019/3/28.
+ *
+ * 与 un-fair 对立
+ */
+public interface Fair extends LockAspect {
+}
