bbec445d432611493eb683a7864f8e0caeac0d7e
yantong <sun18701689022@gmail.com>
2019-03-29 13:59:02 +0800

    linux command 相关命令

--- yt-linux/src/main/java/com/yt/linux/command/NetStat.java
+++ yt-linux/src/main/java/com/yt/linux/command/NetStat.java
@@ -5,5 +5,19 @@ package com.yt.linux.command;
  */
 public class NetStat {
 
-
+    /**
+     * -t
+     *  tcp协议
+     * -u
+     *  udp协议
+     * -a
+     *  all
+     * -n
+     *  TODO
+     * -p
+     *  需要root权限 显示进程id
+     *
+     * 常用：
+     * 查看某个端口 被什么进程占用
+     */
 }
