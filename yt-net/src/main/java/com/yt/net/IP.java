f5831ac098f3110094c85d5a90159c046089d250
yantong <sun18701689022@gmail.com>
2019-03-29 16:20:49 +0800

    ip 外网

--- yt-net/src/main/java/com/yt/net/IP.java
+++ yt-net/src/main/java/com/yt/net/IP.java
@@ -8,6 +8,15 @@ package com.yt.net;
     10.0.0.0/8：      10.0.0.0～10.255.255.255
  　 172.16.0.0/12：   172.16.0.0～172.31.255.255
  　 192.168.0.0/16：  192.168.0.0～192.168.255.255
+
+    获取 本机对外ip：
+    登录网址：
+    ip.cn
+    ipinfo.io
+    cip.cc
+    ifconfig.me
+    myip.ipip.net
+    linux下：curl ipinfo.io即可
  */
 public class IP {
 }
