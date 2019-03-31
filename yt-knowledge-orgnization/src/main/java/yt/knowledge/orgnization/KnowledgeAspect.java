3f16ed4db6320d5374a39e365bb62e756e9b17e4
sun <sun18701689022@gmail.com>
2019-03-31 14:37:02 +0800

    补充认知论方面的个人理解,是这个项目以后的需求

---
+++ yt-knowledge-orgnization/src/main/java/yt/knowledge/orgnization/KnowledgeAspect.java
@@ -0,0 +1,45 @@
+package yt.knowledge.orgnization;/**
+ * Created by SUN on 19/3/31.
+ */
+
+/**
+ * @author SUN
+ * @create 2019-03-31 下午2:07
+ * @desc
+ *
+ * 如果一个对象的状态存在连续性变化,我们的一般做法也是在这些连续的状态上,
+ * 找出基本粒子更小的单位,用离散的状态来近似之,
+ * 所以,我这里所用的面向对象的方法来表达知识点,要求这些知识点的状态,是离散的,
+ * 每个属性在某个状态下,可以取得 确定的、可度量的值
+ *
+ * 这里 为了不引属性这个概念,和java里常用的{@link java.util.Properties}混淆
+ * 提出了方面Aspect的概念:
+ *
+ * the meaning of a knowledge is for use.
+ *
+ * The Aspect for Aspect
+ *
+ * 1.based on time(时代)
+ *      the time decide the base aspect for one knowledge
+ *      base aspect is mutual to each other
+ *
+ *      例如:目前是2019年,本项目提到的一些知识,用于基于x86架构、ip互联网。。。建立的web服务
+ *           本项目分析事物采用的大多数aspect,都是基于当前流行的分析角度。
+ *  explain:
+ *      (10,15)在直角坐标系下,各轴单位长度为1下的一个向量点,
+ *      那它在直角坐标体系但单位长度是5,就会变为(2,3),斜坐标,亦有其他表达。
+ *      aspect:是单位为1或5的横轴或纵轴
+ *      base enviroment,是我们最长用的坐标体系:单位长度为1的直角坐标系
+ *      计算机知识在人的认知描述里,目前可能没有一种显示的精确的定义,所以本项目采用的当下时代,常用的角度
+ *
+ *      换句话说,目前的这个知识点的某个属性,
+ *
+ * ...这些。。。还是等以后再想吧。。。我先糊口一下。。。
+ *
+ *
+ *
+ * */
+public class KnowledgeAspect {
+
+
+}
---
+++ yt-knowledge-orgnization/src/main/java/yt/knowledge/orgnization/KnowledgePointIsObject.java
@@ -0,0 +1,17 @@
+package yt.knowledge.orgnization;/**
+ * Created by SUN on 19/3/31.
+ */
+
+/**
+ * @author SUN
+ * @create 2019-03-31 下午2:07
+ * @desc
+ *
+ * 通常 我们在 设计业务系统时,也是在加深对业务的理解,然后用面向对象的方法来描述之
+ *
+ * 我认为,面向对象是方法,实际也是我们对业务系统的认知、学习、理解
+ *
+ * 同理,我把这种方式,应用于 基于JAVA实现的WEB互联网知识的认知、学习、理解的过程
+ */
+public interface KnowledgePointIsObject {
+}
