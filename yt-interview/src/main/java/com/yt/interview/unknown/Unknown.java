5bd3a0d81fd07ee1e1cfae37a62e28ec3e81608c
sun <sun18701689022@gmail.com>
2019-04-02 19:37:22 +0800

    不知道谁,上来就面试,感觉他就是 抽一口雪茄,问一个问题。。。

---
+++ yt-interview/src/main/java/com/yt/interview/unknown/Unknown.java
@@ -0,0 +1,21 @@
+package com.yt.interview.unknown;/**
+ * Created by SUN on 19/4/2.
+ */
+
+/**
+ * @author SUN
+ * @create 2019-04-02 下午7:36
+ * @desc
+ */
+public class Unknown {
+
+    /**
+     幂等
+     spring boot 配置自动加载
+     ngnix 配置跳转
+     volatile
+     虚拟机对锁的优化
+     redis 数据结构
+     一个线程打印奇数，一个线程打印偶数
+     */
+}
