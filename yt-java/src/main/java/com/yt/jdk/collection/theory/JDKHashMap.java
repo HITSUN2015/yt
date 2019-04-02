7c027e8ec6f5f046b7b2202e4de1dfce98ac6d02
yantong <sun18701689022@gmail.com>
2019-04-02 13:45:48 +0800

    初步理解 hashMap

--- yt-java/src/main/java/com/yt/jdk/collection/theory/JDKHashMap.java
+++ yt-java/src/main/java/com/yt/jdk/collection/theory/JDKHashMap.java
@@ -2,34 +2,40 @@ package com.yt.jdk.collection.theory;/**
  * Created by SUN on 18/3/20.
  */
 
+import com.yt.datastructure.HashTable;
 import com.yt.datastructure.base.tree.RedBlackTree;
 import com.yt.datastructure.base.list.SinglyLinkedList;
 import com.yt.jdk.collection.theory.tread.FailFast机制;
 
+import java.util.HashMap;
+
 /**
  * @author SUN
  * @create 2018-03-20 下午4:04
- * @desc {@link java.util.HashMap}
+ * Created by yantong on 2019/4/2. 改版
+ *
+ * 数据结构：
+ *  元数据： {@link HashMap.Node}
+ *           {@link HashMap.Node#hash}    key的hash值
+ *           {@link HashMap.Node#key}     key对象
+ *           {@link HashMap.Node#value}   value对象
+ *           {@link HashMap.Node#next}    起初落入同一个桶内的元素，做链表
+ *  集合属性：{@link HashMap}
+ *       {@link HashMap#table}      上述数据结构的数组，用来装在Map的实际key-value
+ *       {@link HashMap#entrySet}
+ *       {@link HashMap#size}       集合中元素个数
+ *       {@link HashMap#threshold}  扩容的临街值，一般等于 容量 * {@link HashMap#loadFactor}
+ *       {@link HashMap#loadFactor} 用于计算{@link HashMap#threshold}：实际拥有的数据量，占{@link HashMap#table} 数组的最大值
+ *       {@link HashMap#modCount}   这个字段比较通用，最好的解释见于：{@link java.util.AbstractList#modCount}
+ *                                      个人理解：为了让Iterator遍历时，更快的知道当前数据的变更，更快的返回失败，
+ *                                      每插入一个元素 modCount++ // TODO: 2019/4/2 为什么不用size
+ * 扩容：{@link HashMap#resize()} 触发时间：{@link HashMap#size} > {@link HashMap#threshold}
+ *                                扩容的算法：TODO 再找时间读下 hashMap的扩容
+ * 冲突解决：{@link HashMap#putVal(int, Object, Object, boolean, boolean)}
+ *                  桶的选择：index = ({@link HashMap#table}.length - 1) & {@link Object#hashCode()} 实际就是hash值的低n位
+ *                  冲突：桶内达到{@link HashMap#TREEIFY_THRESHOLD}个，则将这个link转换为tree // TODO: 2019/4/2 再找时间读下节点插入， 与 树的转换
+ *
  */
-public class JDKHashMap implements com.yt.datastructure.HashTable,SinglyLinkedList,RedBlackTree,FailFast机制 {
-
-    /**
-     * Fields:
-     *      Node<K,V>[] table 哈希表中存储了多个链
-     *              1>当单个链中元素超过 TREEIFY_THRESHOLD = 8时,该链转换成红黑树;
-     *      Set<Map.Entry<K,V>> entrySet
-     *      size:
-     *             1>.DEFAULT_INITIAL_CAPACITY = 1 << 4; // aka 16
-     *             2>.二倍扩容
-     *                  1>the elements from each bin must either stay at same index,
-     *                    or move with a power of two offset in the new table
-     *      modCount
-     *      threshold
-     *      loadFactor:
-     *              1>.0.75f
-     *
-     * 这个类还需要看TODO http://blog.csdn.net/u010498696/article/details/45888613
-     */
-
+public class JDKHashMap implements HashTable,SinglyLinkedList,RedBlackTree,FailFast机制 {
 
 }
