ef704742c80cd5e670f3cdde6ed3cced541caeaa
yantong <sun18701689022@gmail.com>
2019-04-02 10:13:07 +0800

    isNaN

---
+++ yt-java/src/main/java/com/yt/jdk/math/Number.java
@@ -0,0 +1,18 @@
+package com.yt.jdk.math;
+
+import com.sun.xml.internal.bind.v2.TODO;
+
+/**
+ * Created by yantong on 2019/4/2.
+ */
+public class Number {
+
+    // TODO: 2019/4/2 不理解NaN
+    public void isNaN() {
+        Float f1 = new Float(0.0 / 0.0);
+        System.out.println(f1.isNaN());
+
+        f1 = new Float(-1.0 / 0.0);
+        System.out.println(f1.isNaN());
+    }
+}
---
+++ yt-java/src/test/java/com/yt/jdk/math/NumberTest.java
@@ -0,0 +1,16 @@
+package com.yt.jdk.math;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by yantong on 2019/4/2.
+ */
+@Test
+public class NumberTest {
+
+    @Test
+    public void testDemo() {
+        Number number = new Number();
+        number.isNaN();
+    }
+}
