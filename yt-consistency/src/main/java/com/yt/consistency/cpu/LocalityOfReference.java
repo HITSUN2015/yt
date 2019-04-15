b6dbc05ad17264179909b0652711246b094cd2c2
yantong <sun18701689022@gmail.com>
2019-04-15 10:48:24 +0800

    建立 consistency 模块

---
+++ yt-consistency/src/main/java/com/yt/consistency/cpu/LocalityOfReference.java
@@ -0,0 +1,15 @@
+package com.yt.consistency.cpu;
+
+/**
+ * Created by yantong on 2019/4/12.
+ *
+ * https://en.wikipedia.org/wiki/Locality_of_reference
+ *
+ * TODO
+ *
+ * 会有个指令预测 https://en.wikipedia.org/wiki/Branch_predictor
+ *
+ * 
+ */
+public interface LocalityOfReference {
+}
---
+++ yt-consistency/src/main/java/com/yt/consistency/cpu/MESI.java
@@ -0,0 +1,8 @@
+package com.yt.consistency.cpu;
+
+/**
+ * Created by yantong on 2019/4/12.
+ */
+public class MESI {
+
+}
